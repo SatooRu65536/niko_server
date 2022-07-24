90ae813757f7ac76bbcd6e8ed1fab46d73e9b613
SatooRu <ryuto65536@gmail.com>
2022-07-24 15:21:22 +0900

    Niko_server 完成

---
+++ src/main.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const admin = require("firebase-admin");
+const moment = require('moment');
+const serviceAccount = require("./serviceAccountKey.json");
+const currentTime = moment();
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://nikocomments-65536-default-rtdb.asia-southeast1.firebasedatabase.app/"
+});
+const db = admin.database();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+    if (!req.body.comment) {
+        res.sendFile(`${__dirname}/index.html`);
+        return
+    }
+
+    const comment = req.body.comment
+    console.log(comment);
+    res.sendFile(`${__dirname}/index.html`);
+
+    const time = currentTime.format("YYYYMMDDHHmmss");
+    db.ref(`nikocomments/${time}`).set({ comment: comment });
+});
+
+app.get("/script.js", (req, res, next) => {
+    res.sendFile(`${__dirname}/script.js`);
+});
+
+app.listen(8000, () => console.log("Server is running ..."));
